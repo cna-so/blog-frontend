4fe3295ff98399311b3eec5aa510d46407647c5d
cna <cna.so@outlook.com>
2023-07-16 12:59:26 +0330

    002 -- add head dialog

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import WideMenu from "@/components/header/WideMenu";
-import Link from "next/link";
+import HeaderMenu from "@/components/header/HeaderMenu";
+import Header from "@/components/header/Header";
 
-const Header = () => {
+const Index = () => {
     return (
-        <header className={"flex justify-between items-center py-4"}>
-            <Link href={"/"} className={"font-bold"}>CLOG</Link>
-            <div className="hidden md:block"><WideMenu items={menuItems}/></div>
-        </header>
+        <div>
+           <HeaderMenu/>
+           <Header/>
+        </div>
     );
 };
 
-const menuItems = [
-    {title: "Home", link: "/"},
-    {title: "Categories", link: "/categories"},
-]
-
-export default Header;
+export default Index;
