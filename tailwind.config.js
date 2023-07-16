e39aff02d0e00c6b96371fa8fd33e9d11f6c4e12
cna <cna.so@outlook.com>
2023-07-16 11:47:08 +0330

    000 -- initial project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors : {
+      "dark-bg" : "#253237",
+      "gray-light" : "#9db4c0",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
