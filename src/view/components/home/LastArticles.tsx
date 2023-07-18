c1b51e55e569f7583591f379aa8e24044407eb28
cna <cna.so@outlook.com>
2023-07-18 15:44:54 +0330

    006 -- fix articles and menu

--- src/view/components/home/LastArticles.tsx
+++ src/view/components/home/LastArticles.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { IPostModels } from "@/models/PostModels";
-import SwiperItem from "@/components/swiper/SwiperItem";
+import React from "react";
+import { IPostProps } from "@/models/PostModels";
+import SwiperItem from "@/view/components/swiper/SwiperItem";
 import { useRouter } from "next/router";
 
-const LastArticles = ({ data }: { data: IPostModels }) => {
+const LastArticles = ({ posts, showMore, next_page }: IPostProps) => {
   const { push, pathname, query } = useRouter();
-  const [articles, setArticles] = useState(data);
   return (
     <main className={"w-full flex flex-col justify-center"}>
       <div className={"grid grid-cols-4 gap-8"}>
-        {articles.posts.map((post) => (
+        {posts?.map((post) => (
           <div key={post._id} className={""}>
             <SwiperItem
               imageUrl={post.cover_url}
@@ -19,14 +18,23 @@ const LastArticles = ({ data }: { data: IPostModels }) => {
           </div>
         ))}
       </div>
-      {(query.page ? +query.page : 1) !== articles.total_page && (
+      {(query.page ? +query.page : 1) <= next_page && (
         <button
-          onClick={() => push("/articles")}
+          onClick={async () => {
+            push(
+              { pathname, query: { page: query.page ? +query.page + 1 : 2 } },
+              undefined,
+              {
+                shallow: true,
+              }
+            );
+            showMore(query.page ? +query.page + 1 : 2);
+          }}
           className={
             "bg-accent-color rounded-full py-2 px-4 mx-auto text-white font-bold text-2xl my-8"
           }
         >
-          All Articles
+          Show More
         </button>
       )}
     </main>
